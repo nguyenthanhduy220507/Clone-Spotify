627ce52c0e5a4063c4400d62f6109266fdce4d0a
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2023-05-18 23:57:43 +0700

    update

--- controllers/collections.php
+++ controllers/collections.php
@@ -2,10 +2,23 @@
 require_once "./config/controller.php";
 class Collections extends Controller
 {
+    private $DB_song;
+    private $DB_user;
+    private $DB_artist;
+    private $DB_album;
+
+    public function __construct()
+    {
+        $this->DB_song = $this->model('SongModel');
+        $this->DB_user = $this->model('UserModel');
+        $this->DB_artist = $this->model('ArtistModel');
+        $this->DB_album = $this->model('AlbumModel');
+    }
     public function collection_like_song()
     {
         $this->view('collections/collection-like-song', [
-
+            'songs' => $this->DB_song->getAll(),
+            'users' => $this->DB_user->getAll(),
         ]);
     }
     public function collection_playlist()
@@ -17,19 +30,19 @@ class Collections extends Controller
     public function collection_episode()
     {
         $this->view('collections/collection-episode', [
-
+            
         ]);
     }
     public function collection_artist()
     {
         $this->view('collections/collection-artist', [
-
+            'artists' => $this->DB_artist->getAll(),
         ]);
     }
     public function collection_album()
     {
         $this->view('collections/collection-album', [
-
+            'albums' => $this->DB_album->getAll(),
         ]);
     }
 }
--- views/collections/collection-like-song.php
+++ views/collections/collection-like-song.php
@@ -1,5 +1,6 @@
-<?php require_once 'style.php'?>
+<?php require_once 'style.php' ?>
 <link rel="stylesheet" href="./assets/css/collection-like-song.css">
+
 <body>
     <div id="main" class="d-grid">
         <header id="top-bar" style="background-color:#4c3693">
@@ -64,12 +65,12 @@
                         </div>
                         <div class="col-md-8" style="margin-top: 100px">
                             <div class="card-body">
-                                <h5 class="card-title text-white">Album</h5>
-                                <p class="card-text text-white fw-bold" style="font-size:60px">21</p>
+                                <h5 class="card-title text-white">Playlist</h5>
+                                <p class="card-text text-white fw-bold" style="font-size:60px">Bài hát đã thích</p>
                                 <div class="d-flex text-white">
                                     <a href="" class="mx-1 text-white hover_a">
-                                        niand zz</a>
-                                    <p class="mx-1"> • 5 bài hát</p>
+                                        <?php echo $data['users'][0]->getUsername() ?></a>
+                                    <p class="mx-1"> • <?php echo count($data['songs']) ?> bài hát</p>
                                 </div>
                             </div>
                         </div>
@@ -105,147 +106,51 @@
                         </div>
                     </div>
                 </div>
-                <div class="container mt-2 data-row">
-                    <div class="container-fuild">
-                        <div class="row my_row">
-                            <div class="col-1 d-flex align-items-center justify-content-center">
-                                <span class="text-white">1</span>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <img src="https://i.scdn.co/image/ab67616d00004851bffc14adc7cc41fbb03bbaa8" class="py-2 img-fluid rounded-start" alt="...">
-                                <div class="ms-3">
-                                    <div class="">
-                                        <a href="" class="card-title fw-bold mt-2 hover_a text-white" style="font-size:13px">Vì yêu cứ đâm đầu</a>
+                <?php $counter =1; ?>
+                <?php foreach ($data['songs'] as $song) { ?>
+                    <div class="container mt-2 data-row">
+                        <div class="container-fuild">
+                            <div class="row my_row">
+                                <div class="col-1 d-flex align-items-center justify-content-center">
+                                    <span class="text-white"><?php echo $counter++; ?></span>
+                                </div>
+                                <div class="col-3 d-flex align-items-center justify-content-left px-0">
+                                    <img src="<?php echo $song->getSongImageUrl() ?>" class="py-2 img-fluid rounded-start" alt="..." style="height: 75px">
+                                    <div class="ms-3">
+                                        <div class="">
+                                            <a href="" class="card-title fw-bold mt-2 hover_a text-white" style="font-size:13px"><?php echo $song->getSongTitle() ?></a>
+                                        </div>
+                                        <div class="d-flex">
+                                            <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b"><?php echo $song->getSongArtist()->getArtistName() ?></a>
+                                        </div>
                                     </div>
-                                    <div class="d-flex">
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">MIN,</a>
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">
-                                            Đen,</a>
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">
-                                            JustaTee</a>
+                                </div>
+                                <div class="col-3 d-flex align-items-center justify-content-left px-0">
+                                    <div class="card-body">
+                                        <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">
+                                            <?php echo $song->getSongAlbum()->getAlbumTitle() ?>
+                                        </a><br>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <div class="card-body">
-                                    <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">Vì Yêu
-                                        cứ Đâm Đầu
-                                    </a><br>
+                                <div class="col-3 d-flex align-items-center justify-content-left px-0">
+                                    <div class="card-body">
+                                        <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">2 tuần trước
+                                        </a><br>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <div class="card-body">
-                                    <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">1 giây
-                                        trước
-                                    </a><br>
+                                <div class="col-2 d-flex align-items-center justify-content-center">
+                                    <div class="col-12 col-md-2 d-flex align-items-center justify-content-end">
+                                        <a href="" class="text-white me-4 icon_heart"><i class="niand-icon-spotify-heart"></i> </a>
+                                        <span class="text-white"><?php echo sprintf("%d:%02d", floor($song->getSongDuration() / 60), $song->getSongDuration() % 60); ?></span>
+                                        <a href="" class="text-white ms-4 icon_three_dot"><i class="niand-icon-spotify-three-dots"></i></a>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-2 d-flex align-items-center justify-content-center">
-                                <div class="col-12 col-md-2 d-flex align-items-center justify-content-end">
-                                    <a href="" class="text-white me-4 icon_heart"><i class="niand-icon-spotify-heart"></i> </a>
-                                    <span class="text-white">
-                                        1:39
-                                    </span>
-                                    <a href="" class="text-white ms-4 icon_three_dot"><i class="niand-icon-spotify-three-dots"></i></a>
-                                </div>
+
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="container mt-2 data-row">
-                    <div class="container-fuild">
-                        <div class="row my_row">
-                            <div class="col-1 d-flex align-items-center justify-content-center">
-                                <span class="text-white">1</span>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <img src="https://i.scdn.co/image/ab67616d00004851bffc14adc7cc41fbb03bbaa8" class="py-2 img-fluid rounded-start" alt="...">
-                                <div class="ms-3">
-                                    <div class="">
-                                        <a href="" class="card-title fw-bold mt-2 hover_a text-white" style="font-size:13px">Vì yêu cứ đâm đầu</a>
-                                    </div>
-                                    <div class="d-flex">
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">MIN,</a>
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">
-                                            Đen,</a>
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">
-                                            JustaTee</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <div class="card-body">
-                                    <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">Vì Yêu
-                                        cứ Đâm Đầu
-                                    </a><br>
-                                </div>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <div class="card-body">
-                                    <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">1 giây
-                                        trước
-                                    </a><br>
-                                </div>
-                            </div>
-                            <div class="col-2 d-flex align-items-center justify-content-center">
-                                <div class="col-12 col-md-2 d-flex align-items-center justify-content-end">
-                                    <a href="" class="text-white me-4 icon_heart"><i class="niand-icon-spotify-heart"></i> </a>
-                                    <span class="text-white">
-                                        1:39
-                                    </span>
-                                    <a href="" class="text-white ms-4 icon_three_dot"><i class="niand-icon-spotify-three-dots"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="container mt-2 data-row">
-                    <div class="container-fuild">
-                        <div class="row my_row">
-                            <div class="col-1 d-flex align-items-center justify-content-center">
-                                <span class="text-white">1</span>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <img src="https://i.scdn.co/image/ab67616d00004851bffc14adc7cc41fbb03bbaa8" class="py-2 img-fluid rounded-start" alt="...">
-                                <div class="ms-3">
-                                    <div class="">
-                                        <a href="" class="card-title fw-bold mt-2 hover_a text-white" style="font-size:13px">Vì yêu cứ đâm đầu</a>
-                                    </div>
-                                    <div class="d-flex">
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">MIN,</a>
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">
-                                            Đen,</a>
-                                        <a href="" class="card-text hover_a" style="font-size:13px; color: #6b6b6b">
-                                            JustaTee</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <div class="card-body">
-                                    <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">Vì Yêu
-                                        cứ Đâm Đầu
-                                    </a><br>
-                                </div>
-                            </div>
-                            <div class="col-3 d-flex align-items-center justify-content-left px-0">
-                                <div class="card-body">
-                                    <a href="#" class="card-title mt-2 text-white hover_a d-xl-flex d-lg-flex d-md-flex d-sm-none d-none" style="font-size:15px">1 giây
-                                        trước
-                                    </a><br>
-                                </div>
-                            </div>
-                            <div class="col-2 d-flex align-items-center justify-content-center">
-                                <div class="col-12 col-md-2 d-flex align-items-center justify-content-end">
-                                    <a href="" class="text-white me-4 icon_heart"><i class="niand-icon-spotify-heart"></i> </a>
-                                    <span class="text-white">
-                                        1:39
-                                    </span>
-                                    <a href="" class="text-white ms-4 icon_three_dot"><i class="niand-icon-spotify-three-dots"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    <?php $counter++; ?>
+                <?php } ?>
             </div>
         </main>
 
@@ -312,4 +217,5 @@
     </div>
 </body>
 <?php require_once 'script.php' ?>
+
 </html>
