f03bd596336f6bf791ace91ed55fafffd8d604b8
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2023-05-18 22:25:45 +0700

    update

--- views/collections/collection-album.php
+++ views/collections/collection-album.php
@@ -1,90 +1,78 @@
-<?php require_once 'style.php';?>
+<?php require_once 'style.php'; ?>
 <link rel="stylesheet" href="./assets/css/index_collecttion_album.css">
 
 <body>
     <div id="main" class="d-grid">
-<?php require_once 'header.php';?>
-        
-
+        <?php require_once 'header.php'; ?>
         <main id="main-view">
             <!-- TODO Nội dung của trang con -->
             <h2 class="m-4 text-white fw-bold">Album</h2>
             <div class="container">
                 <div class="row d-flex justify-content-center align-items-center">
-                    <div class="col-md-2 col-sm-6 col-8 border border-dark hover_div ms-4 mb-4">
+                    <div class="col-md-2 col-sm-6 col-8 border border-dark ms-4 mb-4">
                         <div class="card my-3" style="background-color:#1a1a1a">
                             <div class="card mt-1 px-1" style="background-color:#1a1a1a">
-                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538"
-                                    class="card-img-top" alt="...">
+                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538" class="card-img-top" alt="...">
                             </div>
                             <div class="card-body">
                                 <div class="play-btn-wrapper">
-                                    <a href="#" class="btn play-btn"><i
-                                            class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
+                                    <a href="#" class="btn play-btn"><i class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
                                 </div>
                                 <h6 class="card-title fw-bold text-white">Show của Đen</h6>
                                 <p class="card-text" style="color: #5b5b5b">Đen</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-2 col-sm-6 col-8 border border-dark hover_div ms-4 mb-4">
+                    <div class="col-md-2 col-sm-6 col-8 border border-dark ms-4 mb-4">
                         <div class="card my-3" style="background-color:#1a1a1a">
                             <div class="card mt-1 px-1" style="background-color:#1a1a1a">
-                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538"
-                                    class="card-img-top" alt="...">
+                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538" class="card-img-top" alt="...">
                             </div>
                             <div class="card-body">
                                 <div class="play-btn-wrapper">
-                                    <a href="#" class="btn play-btn"><i
-                                            class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
+                                    <a href="#" class="btn play-btn"><i class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
                                 </div>
                                 <h6 class="card-title fw-bold text-white">Show của Đen</h6>
                                 <p class="card-text" style="color: #5b5b5b">Đen</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-2 col-sm-6 col-8 border border-dark hover_div ms-4 mb-4">
+                    <div class="col-md-2 col-sm-6 col-8 border border-dark ms-4 mb-4">
                         <div class="card my-3" style="background-color:#1a1a1a">
                             <div class="card mt-1 px-1" style="background-color:#1a1a1a">
-                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538"
-                                    class="card-img-top" alt="...">
+                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538" class="card-img-top" alt="...">
                             </div>
                             <div class="card-body">
                                 <div class="play-btn-wrapper">
-                                    <a href="#" class="btn play-btn"><i
-                                            class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
+                                    <a href="#" class="btn play-btn"><i class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
                                 </div>
                                 <h6 class="card-title fw-bold text-white">Show của Đen</h6>
                                 <p class="card-text" style="color: #5b5b5b">Đen</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-2 col-sm-6 col-8 border border-dark hover_div ms-4 mb-4">
+                    <div class="col-md-2 col-sm-6 col-8 border border-dark ms-4 mb-4">
                         <div class="card my-3" style="background-color:#1a1a1a">
                             <div class="card mt-1 px-1" style="background-color:#1a1a1a">
-                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538"
-                                    class="card-img-top" alt="...">
+                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538" class="card-img-top" alt="...">
                             </div>
                             <div class="card-body">
                                 <div class="play-btn-wrapper">
-                                    <a href="#" class="btn play-btn"><i
-                                            class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
+                                    <a href="#" class="btn play-btn"><i class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
                                 </div>
                                 <h6 class="card-title fw-bold text-white">Show của Đen</h6>
                                 <p class="card-text" style="color: #5b5b5b">Đen</p>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-2 col-sm-6 col-8 border border-dark hover_div ms-4 mb-4">
+                    <div class="col-md-2 col-sm-6 col-8 border border-dark ms-4 mb-4">
                         <div class="card my-3" style="background-color:#1a1a1a">
                             <div class="card mt-1 px-1" style="background-color:#1a1a1a">
-                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538"
-                                    class="card-img-top" alt="...">
+                                <img src="https://i.scdn.co/image/ab67616d00001e024888abe8ee4d110278a67538" class="card-img-top" alt="...">
                             </div>
                             <div class="card-body">
                                 <div class="play-btn-wrapper">
-                                    <a href="#" class="btn play-btn"><i
-                                            class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
+                                    <a href="#" class="btn play-btn"><i class="znake-icon-spotify-play fs-5 hightlight1"> </i></a>
                                 </div>
                                 <h6 class="card-title fw-bold text-white">Show của Đen</h6>
                                 <p class="card-text" style="color: #5b5b5b">Đen</p>
@@ -163,8 +151,7 @@
                     <div id="now-playing-bar-left" class="col d-flex align-items-center">
                         <div class="d-flex gap-3 justify-content-start align-items-center">
                             <div>
-                                <img class="img-fluid rounded-1"
-                                    src="https://i.scdn.co/image/ab67616d0000485170cb943c9a67b7eda3414366" alt="">
+                                <img class="img-fluid rounded-1" src="https://i.scdn.co/image/ab67616d0000485170cb943c9a67b7eda3414366" alt="">
                             </div>
                             <div>
                                 <div class="title">
@@ -186,8 +173,7 @@
                     <div id="now-playing-bar-center" class="col d-flex align-items-center">
                         <div class="d-flex flex-column w-100 gap-2">
                             <div class="player-controls d-flex align-items-center justify-content-center gap-4">
-                                <div
-                                    class="player-controls-left d-flex align-items-center justify-content-center gap-4">
+                                <div class="player-controls-left d-flex align-items-center justify-content-center gap-4">
                                     <button type="button">
                                         <i class="niand-icon-spotify-mix"></i>
                                     </button>
@@ -196,13 +182,11 @@
                                     </button>
                                 </div>
                                 <div class="player-controls-center">
-                                    <button type="button"
-                                        class="bg-white m-0 p-1 rounded-circle d-flex justify-content-center align-items-center">
+                                    <button type="button" class="bg-white m-0 p-1 rounded-circle d-flex justify-content-center align-items-center">
                                         <i class="niand-icon-spotify-play text-black"></i>
                                     </button>
                                 </div>
-                                <div
-                                    class="player-controls-right d-flex align-items-center justify-content-center gap-4">
+                                <div class="player-controls-right d-flex align-items-center justify-content-center gap-4">
                                     <button type="button">
                                         <i class="niand-icon-spotify-next"></i>
                                     </button>
@@ -248,36 +232,33 @@
     var cardBodies = document.querySelectorAll('.card-body');
 
     // Lặp qua từng phần tử card-body
-    if(cardBodies !=null){
-        cardBodies.forEach(function (cardBody) {
-        // Lấy phần tử chứa nút "play" của từng card-body
-        var playBtn = cardBody.querySelector('.play-btn');
+    if (cardBodies != null) {
+        cardBodies.forEach(function(cardBody) {
+            // Lấy phần tử chứa nút "play" của từng card-body
+            var playBtn = cardBody.querySelector('.play-btn');
 
-        // Khi hover vào card-body
-        cardBody.addEventListener('mouseover', function () {
-            // Thêm lớp "show" vào phần tử chứa nút "play"
-            playBtn.classList.add('show');
-        });
+            // Khi hover vào card-body
+            cardBody.addEventListener('mouseover', function() {
+                // Thêm lớp "show" vào phần tử chứa nút "play"
+                playBtn.classList.add('show');
+            });
 
-        // Khi rời chuột khỏi card-body
-        cardBody.addEventListener('mouseout', function () {
-            // Loại bỏ lớp "show" khỏi phần tử chứa nút "play"
-            playBtn.classList.remove('show');
+            // Khi rời chuột khỏi card-body
+            cardBody.addEventListener('mouseout', function() {
+                // Loại bỏ lớp "show" khỏi phần tử chứa nút "play"
+                playBtn.classList.remove('show');
+            });
         });
-    });
     }
 
-    window.onload = function () {
-        document.getElementById("open-btn").addEventListener('click', function () {
+    window.onload = function() {
+        document.getElementById("open-btn").addEventListener('click', function() {
             document.getElementById("side-bar").classList.toggle('d-sm-none');
             document.getElementById("side-bar").classList.toggle('d-none');
             document.getElementById("side-bar").style.width = '85vw';
             document.getElementById("main-view").classList.toggle('d-none');
         })
     }
-
-
-
 </script>
 
 </html>
