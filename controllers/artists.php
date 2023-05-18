5b5351b0d8bc07c33031f54fdb5c46ffea3b0248
congdang102 <95157465+congdang102@users.noreply.github.com>
2023-05-18 16:15:57 +0700

    update

--- controllers/artists.php
+++ controllers/artists.php
@@ -2,29 +2,31 @@
 require_once "./config/controller.php";
 class Artists extends Controller
 {
-    public function artst_login($id)
+    public function artist_login($id)
     {
+        
         $DB = $this->model('ArtistModel');
-        $current_artist = $DB->getByID($id);
+        $current_artist= $DB->getByID($id);
         $DB_song = $this->model('SongModel');
         $list_song = [];
+        $DB_album = $this->model('AlbumModel');
         $list_album = [];
         //song
         foreach ($DB_song->getALL() as $song) {
-            if ($song->getSongAlbum()->getAlbumId() == $current_artist->getAlbumId()) {
+            if ($song->getSongArtist()->getArtistId() == $current_artist->getArtistId()) {
                 $list_song[] = $song;
             }
         }
         //album
-        foreach ($DB->getALL() as $album) {
-            if($album->getAlbumArtist()->getArtistId() == $current_artist->getAlbumArtist()->getArtistId() && $album->getAlbumId() != $current_artist->getAlbumId()) {
+        foreach ($DB_album->getALL() as $album) {
+            if($album->getAlbumArtist()->getArtistId() == $current_artist->getArtistId()) {
                 $list_album[] = $album;
             }
         }
 
-        $this->view('albums/album-login', [
+        $this->view('artists/artist_login', [
             'id' => $id,
-            'album'=>$current_artist,
+            'artist'=>$current_artist,
             'albums'=>$list_album,
             'song_num'=>count($list_song),
             'songs' =>$list_song
--- controllers/playlists.php
+++ controllers/playlists.php
@@ -9,11 +9,18 @@ class Playlists extends Controller
     public function playlist($id)
     {
        
-        $DB = $this->model("PlaylistModel");
-        $this->view('playlists/playlist', [
-            'playlist' => $DB->getByID($id)
-
-        ]);
+        $DB = $this->model('PlaylistModel');
+        $current_playlist= $DB->getByID($id);
+        $DB_song = $this->model('SongModel');
+        $list_song = [];
+        $DB_album = $this->model('AlbumModel');
+        $list_album = [];
+        //song
+        foreach ($DB_song->getALL() as $song) {
+            if ($song->getSongArtist()->getArtistId() == $current_artist->getArtistId()) {
+                $list_song[] = $song;
+            }
+        }
     }
 
     public function playlist_login($id)
