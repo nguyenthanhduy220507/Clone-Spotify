fc74ce15c3d204e387213fed87258cabaaa5cf0f
Nguyễn Thanh Duy <nguyenthanhduy220507@gmail.com>
2023-05-19 00:13:15 +0700

    update

--- views/home/index_login.php
+++ views/home/index_login.php
@@ -1,8 +1,8 @@
 <?php
     require_once "./config/basehref.php";
     $url = getUrl();
-    if (isset($_SESSION['username'])) {
-        header("Location: ?url=home/index_login");
+    if (!isset($_SESSION['username'])) {
+        header("Location: ?url=home/index");
     }
 ?>
 <!DOCTYPE html>
